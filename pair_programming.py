8c93b357c567bc95130e25667d80bd751320ec2a
Madison <Madison_schlarman@mines.edu>
2026-03-31 13:36:46 -0600

    add feedback and test function

--- pair_programming.py
+++ pair_programming.py
@@ -7,3 +7,9 @@ def polar_to_cartesian(r, theta):
     x = r * math.cos(theta)
     y = r * math.sin(theta)
     return x, y
+
+#Madison's Commentary:
+#There are not any comments on the function, but it is also a pretty short/simple function.
+#The function accepts polar coordinates in radius and angle theta, then uses the math import
+#and the algebraic equations for converting to cartesian coordinates to convert the given values
+#into x and y values.
